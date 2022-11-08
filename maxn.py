60ed04e65c101612fbc3b071eae7aba2c6f231f5
Vikash Yadav <incrediblevry0611@gmail.com>
2022-11-08 23:39:41 +0530

    ai-is-born-happy-bdayy

--- maxn.py
+++ maxn.py
@@ -1,48 +1,56 @@
 import numpy as np
 
-# returns utility value
-def utility(turn):
-  ut = np.zeros(p)
-  ut[(turn-1)%p] = 1
-  print("utility:", ut)
-  return ut
+def utility(turn,p):
+    """returns utility array for leaf node"""
+    ut = np.zeros(p)
+    ut[(turn - 1) % p] = 1
+    # print("utility:", ut)
+    return ut, -1,-1
 
-# returns boolean 
 def is_leaf(rows):
-  return np.sum(rows)==0
+    """returns true if leaf node"""
+    return np.sum(rows) == 0
 
-p = 3 # number of players
 # turn indexing from 0
+def maxn(rows, turn,p):
+    """returns best move for a given state"""
+    res,best_row,best_sticks = max_val(rows, turn,p)
+    # print("besti", besti, "bestj", bestj)
+    return (best_row,best_sticks)
 
-def max_val(rows,turn):
-  print("curr",rows,"turn", turn)
-  if is_leaf(rows):
-    print("leafff")
-    return utility(turn)
-  res_val = -1000
-  res = res_val * np.ones(p)
-  print(rows)
-  
-  for i in range(len(rows)):
-    c=1
-    for j in range(1,rows[i]+1):
-      row1 = rows.copy()
-      row1[i] -= j
-      res_temp = max_val(row1, (turn+1) % p)
-      if res_val > res_temp[turn]:
-        continue
-      elif res_val < res_temp[turn]:
-        res= res_temp
-        res_val = res_temp[turn]
-      else:
-        print("gon for summ")
-        res+=res_temp
-        c+=1
-      print("i",i,"j",j)
-      print("res",res)
-    res/=c
-    print("res", res)
-  return res
-  
-rows = np.array([1,2])
-max_val(rows,0)
+def max_val(rows, turn,p):
+    # print("curr", rows, "turn", turn)
+    if is_leaf(rows):
+        # print("leafff")
+        return utility(turn,p)
+    res_val = -1000
+    res = res_val * np.ones(p)
+    # print(rows)
+    best_row,best_sticks = 1,1
+
+    for i in range(len(rows)):
+        c = 1
+        for j in range(1, rows[i] + 1):
+            new_rows = rows.copy()
+            new_rows[i] -= j
+            res_temp,bla1,bla2 = max_val(new_rows, (turn + 1) % p,p)
+            # print("res_temp", res_temp)
+            if res_val > res_temp[turn]:
+                continue
+            elif res_val < res_temp[turn]:
+                best_row,best_sticks = i+1,j
+                res = res_temp
+                res_val = res_temp[turn]
+            else:
+                # print("gon for summ")
+                res += res_temp
+                c += 1
+            # print("i", i, "j", j)
+            # print("res", res)
+        res /= c
+        # print("res", type(res))
+    return res,best_row,best_sticks
+
+if __name__ == "__main__":
+    rows = np.array([1, 2])
+    max_val(rows, 0,3)
--- nim.py
+++ nim.py
@@ -1,53 +1,57 @@
-import random
 import numpy as np
+import maxn
+import os
 
-game_flow = "n"
-while game_flow == "n":
-
-    print("PLAY A HUMAN VS. HUMAN NIM GAME\n")
-    print("NOTE- The player to remove the last stick wins")
-    p = int(input("Enter number of players: "))  # number of players
-    r = int(input("Enter number of rows: "))  # number of rows
-    turn = 0
-    rows = np.random.choice(range(3, 15), size=(r))
-
+def print_rows():
+    os.system("cls")
     print("")
     for i in range(r):
         print(chr(11007) * rows[i], " (", rows[i], ")", sep="")
     print("")
 
-    while True:
+def get_human_inputs():
+    row = int(input("Enter the row to remove sticks: "))
+    num = int(input("Enter how many sticks to remove: "))
+    return row,num
 
-        print(turn % p + 1, "'S TURN:", sep="")
-        row = int(input("Enter the row to remove sticks: "))
-        num = int(input("Enter how many sticks to remove: "))
+def get_ai_inputs():
+    row,num = maxn.maxn(rows, turn,p)
+    return row,num
 
-        if row <= r and rows[row-1] != 0:
-            if num > rows[row-1] or num <= 0:
-                print("\nINVALID NUMBER. Please enter a valid number.\n")
-                continue
-            rows[row-1] -= num
 
-        else:
-            print("\nINVALID ROW. Please enter valid row\n")
+print("PLAY A MULTIPLAYER NIM GAME\n")
+print("NOTE- The player to remove the last stick wins")
+p = int(input("Enter number of human players: "))  # number of players
+r = int(input("Enter number of rows: "))  # number of rows
+rows = np.random.choice(range(3, 5), size=(r))
+
+turn = 0
+p+=1
+while True:
+
+    print_rows()
+    if turn == p - 1:
+        print("AI's turn:")
+        row,num = get_ai_inputs()
+        print(f"AI chooses to remove {num} sticks from row {row}")
+        input("Press enter to continue...")
+    else:
+        print(f"PLAYER {turn + 1}'S TURN:", sep="")
+        row,num = get_human_inputs()
+
+    if row <= r and rows[row - 1] != 0:
+        if num > rows[row - 1] or num <= 0:
+            print("\nINVALID NUMBER. Please enter a valid number.\n")
             continue
+        rows[row-1] -= num
+    else:
+        print("\nINVALID ROW. Please enter valid row\n")
+        continue
 
-        print("")
-        for i in range(r):
-            print(chr(11007) * rows[i], " (", rows[i], ")", sep="")
-        print("")
+    if np.sum(rows) == 0:
+        print_rows()
+        res_string = "AI" if turn==p-1 else f"PLAYER {turn+1}"
+        print(res_string, " WINS!", sep="")
+        break
 
-        if np.sum(rows) == 0:
-            print(turn % p + 1, "WINS!", sep="")
-            break
-
-        turn += 1
-
-    print("\n", "-" * 70, "\n", end="", sep="")
-    game_flow = input(
-        "\nEnter n to play a new game or press q to quit the game:")
-    print("\n", "-" * 70, "\n", end="", sep="")
-    print("-" * 70, "\n" * 2, end="")
-
-print("THANK YOU! GOODBYE")
-input("Press ENTER  to exit")
+    turn = (turn+1)%p
